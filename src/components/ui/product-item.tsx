a2e5cf4fe3c681d3d0b1ee142e40ad7c0a981b2a
Iago Dantas <iagodantas.8@gmail.com>
2023-11-02 03:11:13 -0300

    feat: add ProductItem and ProductList components

---
+++ src/components/ui/product-item.tsx
@@ -0,0 +1,56 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import { Badge } from "./badge";
+import { ArrowDownIcon } from "lucide-react";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex max-w-[156px] flex-col gap-4">
+      <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          alt={product.name}
+          className="h-auto w-auto max-w-[80%] max-h-[70%]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+        {product.discountPercent > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDownIcon size={14} /> {product.discountPercent}%
+          </Badge>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercent > 0 ? (
+            <>
+              <p className="font-semibold">R$ {product.totalPrice.toFixed(2)}</p>
+
+              <p className="line-through opacity-75 text-xs">R$ {Number(product.basePrice.toFixed(2))}</p>
+            </>
+          ) : (
+
+            <p className="line-through opacity-75 text-xs">R$ {Number(product.basePrice.toFixed(2))}</p>
+          )
+          }
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
