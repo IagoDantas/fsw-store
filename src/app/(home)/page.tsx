3542fffabfe33264b8710c2ebda5027d61a88d30
Iago Dantas <iagodantas.8@gmail.com>
2023-11-02 23:25:46 -0300

    feat: add second banner to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,17 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="pl-5 font-bold  uppercase mb-5">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto em mouses"
+        className="h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   )
 }
