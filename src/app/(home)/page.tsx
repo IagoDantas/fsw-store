9bdf218abae4cce9f530aa9719d6371906e36b10
Iago Dantas <iagodantas.8@gmail.com>
2023-11-02 23:56:18 -0300

    feat: add mouse section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,6 +29,14 @@ export default async function Home() {
     }
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses',
+      },
+    }
+  });
+
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner
@@ -58,6 +66,10 @@ export default async function Home() {
           src="/banner-home-03.png"
           alt="Até 55% de desconto em mouses"
         />
+      </div>
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   )
